37848244494412d91a0dea7460499a65d51f3d62
Abdullah Zayed <41540861+xaadu@users.noreply.github.com>
2020-04-30 01:21:58 +0600

    Furnished UI

--- js/app.js
+++ js/app.js
@@ -159,6 +159,12 @@ var UIController = (function () {
         return int+'.'+dec
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i=0; i<list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
         getInput: function () { 
             return {
@@ -212,12 +218,6 @@ var UIController = (function () {
         displayPercentages: function (percentages) {
             var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-                for (var i=0; i<list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
                 current.textContent = percentages[index];
                 if (percentages[index]>0)
@@ -232,6 +232,18 @@ var UIController = (function () {
             month = now.getMonth();
             year = now.getFullYear();
             document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+
+        changedType: function() {
+            var fields = document.querySelectorAll(
+                DOMStrings.inputType + ',' +
+                DOMStrings.inputDescription + ',' +
+                DOMStrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+            document.querySelector(DOMStrings.inputBtn).classList.toggle('red');
         },
 
         getDOMStrings: function () {  
@@ -258,6 +270,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
 
